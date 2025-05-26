bb10f3b7f1ee8abba097f6a57682bed5d7d844cf
Claudia Montero Ramírez <131159036+clmonter@users.noreply.github.com>
2025-05-26 16:47:25 +0200

    Update plot_triangle.py

--- src/plot_triangle.py
+++ src/plot_triangle.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 
 def plot_simple_triangle(show=True):
 
@@ -42,33 +43,32 @@ def plot_triangle_lines(show=True):
     color_left = 'red'
     color_right = 'green'
 
-    # Vértices del triángulo
     A = np.array([0, 0])
     B = np.array([1, 0])
     C = np.array([0.5, 1])
 
+    mpl.rcParams['mathtext.fontset'] = 'cm'
+    mpl.rcParams['font.family'] = 'serif'
+    
     plt.figure()
-
-    # Dibujar triángulo
+    
     triangle_x = [A[0], B[0], C[0], A[0]]
     triangle_y = [A[1], B[1], C[1], A[1]]
     plt.plot(triangle_x, triangle_y, 'k-', linewidth=1.5)
-    #plt.fill(triangle_x, triangle_y, 'skyblue', alpha=0.5)
-    #plt.scatter([A[0], B[0], C[0]], [A[1], B[1], C[1]], color='red')
 
-    # === EJE 1: base AB ===
     plt.plot([A[0], B[0]], [A[1], B[1]], color=color_down, linewidth=2)
     draw_parallel_lines_along_direction(A, B, C, num_lines=8, color=color_down)
-
-    # === EJE 2: lado izquierdo AC ===
     plt.plot([A[0], C[0]], [A[1], C[1]], color=color_left, linewidth=2)
     draw_parallel_lines_along_direction(A, C, B, num_lines=8, color=color_left)
-
-    # === EJE 3: lado derecho BC ===
     plt.plot([B[0], C[0]], [B[1], C[1]], color=color_right, linewidth=2)
     draw_parallel_lines_along_direction(B, C, A, num_lines=8, color=color_right)
 
-    # Estética
+    plt.text(0.5, -0.05, r"$\Delta H'_{P_{X} \cdot P_{Y}}$", ha='center', va='center', fontsize=15)
+    plt.text(0.15, 0.5, r"$VI'_{P_{XY}}$", ha='center', va='center', fontsize=15, rotation=65)
+    plt.text(1-0.15, 0.5, r"$2 \cdot MI'_{P_{XY}}$", ha='center', va='center', fontsize=15, rotation=-65)
+
+    plt.axis('off') 
+
     plt.title('Entropy triangle')
     plt.axis('equal')
     plt.grid(False)
@@ -78,32 +78,22 @@ def plot_triangle_lines(show=True):
     if show == True:
         plt.show()
 
-
 def interpolate(p1, p2, t):
-    """Interpolación lineal entre dos puntos"""
     return (1 - t) * np.array(p1) + t * np.array(p2)
 
 def draw_parallel_lines_along_direction(start_vertex1, start_vertex2, opp_vertex, num_lines, color):
-    """
-    Dibuja líneas paralelas al eje definido por start_vertex1 y start_vertex2,
-    empezando desde interpolaciones entre esos vértices y el vértice opuesto.
-    """
     for t in np.linspace(0, 1, num_lines):
-        # Punto a lo largo del eje opuesto
         p1 = interpolate(start_vertex1, opp_vertex, t)
         p2 = interpolate(start_vertex2, opp_vertex, t)
         plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linestyle='--', color=color, linewidth=1)
 
-
 def plot_little_cm(conf_matrix, cmap):
-
     plt.figure(figsize=(3, 3))
     plt.imshow(conf_matrix, cmap=cmap, interpolation='nearest')
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
     plt.show()
-
 
 #################### Plot dark coordinates ####################
 
@@ -128,28 +118,28 @@ def plot_dark_triangle_lines(show=True, title='Entropy triangle'):
     B = np.array([1, 0])
     C = np.array([0.5, 1])
 
+    mpl.rcParams['mathtext.fontset'] = 'cm'
+    mpl.rcParams['font.family'] = 'serif'
+
     plt.figure()
 
-    # Dibujar triángulo
     triangle_x = [A[0], B[0], C[0], A[0]]
     triangle_y = [A[1], B[1], C[1], A[1]]
     plt.plot(triangle_x, triangle_y, 'k-', linewidth=1.5)
-    #plt.fill(triangle_x, triangle_y, 'skyblue', alpha=0.5)
-    #plt.scatter([A[0], B[0], C[0]], [A[1], B[1], C[1]], color='red')
 
-    # === EJE 1: base AB ===
     plt.plot([A[0], B[0]], [A[1], B[1]], color=color_down, linewidth=2)
     draw_parallel_lines_along_direction(A, B, C, num_lines=8, color=color_down)
-
-    # === EJE 2: lado izquierdo AC ===
     plt.plot([A[0], C[0]], [A[1], C[1]], color=color_left, linewidth=2)
     draw_parallel_lines_along_direction(A, C, B, num_lines=8, color=color_left)
-
-    # === EJE 3: lado derecho BC ===
     plt.plot([B[0], C[0]], [B[1], C[1]], color=color_right, linewidth=2)
     draw_parallel_lines_along_direction(B, C, A, num_lines=8, color=color_right)
+    
+    plt.text(0.5, -0.05, r"$\Delta H'_{P_{X} \cdot P_{Y}}$", ha='center', va='center', fontsize=15)
+    plt.text(0.15, 0.5, r"$VI'_{P_{XY}}$", ha='center', va='center', fontsize=15, rotation=65)
+    plt.text(1-0.15, 0.5, r"$2 \cdot MI'_{P_{XY}}$", ha='center', va='center', fontsize=15, rotation=-65)
 
-    # Estética
+    plt.axis('off') 
+    
     plt.title(f'{title}')
     plt.axis('equal')
     plt.grid(False)
